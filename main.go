d91c71639dc97c81f695fdc46c90def1753398ae
brianBig <briannbig@gmail.com>
2022-06-12 10:49:11 +0300

    basic REST API with gin

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
-import(
+import (
 	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func main()  {
-	fmt.Println("Go Kenyan Counties - Know kenya movement")
+func main() {
+	fmt.Println("Go Kenyan Counties - Know Kenya movement")
+	r := gin.New()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Go Kenyan Counties - Know Kenya movement",
+		})
+	})
+	r.Run()
+
 }
